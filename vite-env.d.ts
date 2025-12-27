f77fa3b32110a34fa323380eeeabf14e8277e8f3
Ronald Rodriguez <rodruizronald@gmail.com>
2025-12-27 16:33:50 -0600

    supabase client service

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -5,6 +5,8 @@ interface ImportMetaEnv {
   readonly VITE_LOG_LEVEL: string
   readonly VITE_LOG_ENDPOINT: string
   readonly VITE_LOG_API_KEY: string
+  readonly VITE_SUPABASE_URL: string
+  readonly VITE_SUPABASE_PUBLISHABLE_KEY: string
 }
 
 interface ImportMeta {
