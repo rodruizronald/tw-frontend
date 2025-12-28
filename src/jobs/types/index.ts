f4d65376138c183ff379aec6d486bcab04f90be9
Ronald Rodriguez <rodruizronald@gmail.com>
2025-12-27 19:20:31 -0600

    update supabase types

--- src/jobs/types/index.ts
+++ src/jobs/types/index.ts
@@ -47,22 +47,40 @@ export {
   WORK_MODES,
 } from './enums'
 
+// Models - also export PaginationInfo
+export type { PaginationInfo } from './models'
+
 // Filters and search parameters
 export type {
-  ActiveFilterKey,
-  FilterState,
+  DatePreset,
+  FilterKey,
+  GetCompaniesRpcParams,
   JobSearchFilters,
   JobSearchPagination,
   JobSearchParams,
+  MultiSelectFilterKey,
   SearchJobsRpcParams,
+  SingleSelectFilterKey,
 } from './filters'
 export {
+  // Utility functions
   calculateOffset,
   calculateTotalPages,
   clearFilters,
   countActiveFilters,
+  countFilterSelections,
+  // Constants
+  DATE_PRESETS,
+  // Conversion functions
+  datePresetToRange,
+  filtersToURLParams,
   getDefaultPagination,
   hasActiveFilters,
   hasMorePages,
+  MULTI_SELECT_FILTER_KEYS,
+  SINGLE_SELECT_FILTER_KEYS,
+  toGetCompaniesRpcParams,
+  toggleFilterValue,
   toSearchJobsRpcParams,
+  urlParamsToFilters,
 } from './filters'
